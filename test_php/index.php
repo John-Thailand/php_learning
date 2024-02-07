2e5245556a62f61ad3e9cc5b843d49b75aa3e57a
John-Thailand <naoki.honda@mirrorfit.jp>
2024-02-07 17:30:26 +0900

    条件分岐について学習

--- test_php/index.php
+++ test_php/index.php
@@ -1,53 +1,12 @@
 <?php
 
-$array_member = [
-    'name' => '本田',
-    'height' => 170,
-    'hobby' => 'サッカー'
-];
+$height = '90';
 
-echo $array_member['hobby'];
+if ($height === 90) {
+    echo '身長は' . $height . 'cmです';
+}
 
-$array_member_2 = [
-    '本田' => [
-        'height' => 170,
-        'hobby' => 'サッカー'
-    ],
-    '香川' => [
-        'height' => 165,
-        'hobby' => 'サッカー'
-    ]
-];
-
-echo $array_member_2['香川']['height'];
-
-echo '<pre>';
-var_dump($array_member_2);
-echo '</pre>';
-
-$array_member_3 = [
-    '1kumi' => [
-        '本田' => [
-            'height' => 170,
-            'hobby' => 'サッカー'
-        ],
-        '香川' => [
-            'height' => 165,
-            'hobby' => 'サッカー'
-        ]
-    ],
-    '2kumi' => [
-        '長友' => [
-            'height' => 160,
-            'hobby' => 'サッカー'
-        ],
-        '乾' => [
-            'height' => 168,
-            'hobby' => 'サッカー'
-        ],
-    ]
-];
-
-echo $array_member_3['2kumi']['長友']['height'];
+// == 一致
+// === 型も一致
 
 ?>
