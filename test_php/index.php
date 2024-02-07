51b0cdb76956419fea8fd3a9c79da7bc4f451f22
John-Thailand <naoki.honda@mirrorfit.jp>
2024-02-07 20:50:45 +0900

    if elseについて学習

--- test_php/index.php
+++ test_php/index.php
@@ -2,8 +2,20 @@
 
 $height = '90';
 
-if ($height === 90) {
+if ($height === 91) {
     echo '身長は' . $height . 'cmです';
+} else {
+    echo '身長は' . $height . 'cmではありません'
+}
+
+$signal = 'red';
+
+if ($signal === 'red') {
+    echo 'とまれ';
+} else if ($signal === 'yellow') {
+    echo '一旦停止';
+} else {
+    echo '進む';
 }
 
 // == 一致
