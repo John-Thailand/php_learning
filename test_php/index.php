2a26fae03438a9fd9b07555a3d27660a2075f17a
John-Thailand <naoki.honda@mirrorfit.jp>
2024-02-09 16:37:21 +0900

    array_pushについて学習

--- test_php/index.php
+++ test_php/index.php
@@ -1,37 +1,11 @@
 <?php
 
-// 文字列の長さ
+$array = ['りんご', 'みかん'];
 
-$text = 'あいうえお';
+array_push($array, 'ぶどう', 'なし');
 
-// echo strlen($text);
-
-// echo mb_strlen($text);
-
-// 文字列の置換
-
-$str = '文字列を置換します';
-
-// echo str_replace('置換', 'ちかん', $str);
-
-// 指定文字列で分割
-
-$str_2 = '指定文字列で、分割します';
-
-// echo '<pre>';
-// var_dump(explode('、', $str_2));
-// echo '</pre>';
-
-// 正規表現
-
-$str_3 = '特定の文字列が含まれるか確認する';
-
-// echo preg_match('/文字列/', $str_3);
-
-// 指定文字列から文字列を取得する
-
-// echo substr('abcde', 1);
-
-// echo mb_substr('かきくけこ', 1);
+echo '<pre>';
+var_dump($array);
+echo '</pre>';
 
 ?>
