a84390337ecd86a83422c24d3c60923965e322b9
John-Thailand <naoki.honda@mirrorfit.jp>
2024-02-10 21:19:31 +0900

    ストリーム型で書き込みを行う

--- mainte/test.php
+++ mainte/test.php
@@ -2,26 +2,12 @@
 
 $contactFile = '.contact.dat';
 
-// ファイル丸ごと読み込み
-$fileContents = file_get_contents($contactFile);
+$contents = fopen($contactFile, 'a+');
 
-// echo $fileContents;
+$addText = '1行追記' . "\n";
 
-// ファイルに書き込み（上書き）
-// file_put_contents($contactFile, 'テストです');
+fwrite($contents, $addText);
 
-$addText = 'テストです' . "\n";
+fclose($contents);
 
-// ファイルに書き込み（追記）
-file_put_contents($contactFile, $addText, FILE_APPEND);
-
-// 配列 file ,区切る explode, foreach
-
-$allData = file($contactFile);
-
-foreach($allData as $lineData) {
-    $lines = explode(',', $lineData);
-    echo $lines[0]. '<br>';
-    echo $lines[1]. '<br>';
-    echo $lines[2]. '<br>';
-}
+?>
