bc14322b392463791d3fe37bbe5b20af444fdbdf
John-Thailand <naoki.honda@mirrorfit.jp>
2024-02-12 19:42:22 +0900

    Composer（ライブラリ管理ツール）の使い方を学習

---
+++ composertest/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a7c03dbfea02840f62304a2c2c3fd8f2d1c110d3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a7c03dbfea02840f62304a2c2c3fd8f2d1c110d3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
