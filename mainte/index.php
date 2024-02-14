225eceed3c90fcdd88b96a7000b69ef2a564a995
John-Thailand <naoki.honda@mirrorfit.jp>
2024-02-14 20:36:55 +0900

    PDOトランザクション

--- mainte/index.php
+++ mainte/index.php
@@ -23,3 +23,18 @@ $result = $stmt->fetchall();
 echo '<pre>';
 var_dump($result);
 echo '</pre>';
+
+// トランザクション まとまって処理 beginTransaction, commit, rollback
+
+$pdo->beginTransaction();
+
+try {
+    // SQL処理
+    $stmt = $pdo->prepare($sql); // プリペアードステートメント
+    $stmt->bindValue('id', 5, PDO::PARAM_INT); // 紐付け
+    $stmt->execute(); // 実行
+    
+    $pdo->commit();
+} catch(PDOException $e) {
+    $pdo->rollback(); // 更新のキャンセル
+}
