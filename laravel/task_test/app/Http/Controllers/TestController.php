eff64141be036643771f90a79c426f64f3eccfd6
John-Thailand <naoki.honda@mirrorfit.jp>
2024-02-20 14:15:58 +0900

    ルーティング→コントローラ（データ取得する）→ビュー

--- laravel/task_test/app/Http/Controllers/TestController.php
+++ laravel/task_test/app/Http/Controllers/TestController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Test;
 
 class TestController extends Controller
 {
     public function index()
     {
+        // Testの表を全件で取得する
+        $values = Test::all();
+
+        // 取得してきたデータをdd（die + var_dump）で中身を確認する
+        // dd($values);
+
         // viewのtestフォルダのtest.blade.phpへ遷移する
-        return view('tests.test');
+        // compactを使うことで、その変数をviewに渡すことができる
+        return view('tests.test', compact('values'));
     }
 }
