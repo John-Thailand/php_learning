b9727589a71159113e44c3cc032dbc0a4f102d45
John-Thailand <naoki.honda@mirrorfit.jp>
2024-02-20 14:53:36 +0900

    クエリビルダでSQL文のようにDBの操作ができる

--- laravel/task_test/app/Http/Controllers/TestController.php
+++ laravel/task_test/app/Http/Controllers/TestController.php
@@ -4,23 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Test;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
     public function index()
     {
+        // Eloquentを使ってデータ操作を行う方法
         // Testの表を全件で取得する
-        $values = Test::all();
+        // $values = Test::all();
 
-        $count = Test::count();
+        // $count = Test::count();
 
         // idが1のものを取得する
-        $first = Test::findOrFail(1);
+        // $first = Test::findOrFail(1);
 
-        $whereBBB = Test::where('text', '=', 'bbb')->get();
+        // $whereBBB = Test::where('text', '=', 'bbb')->get();
 
         // 取得してきたデータをdd（die + var_dump）で中身を確認する
-        dd($values, $count, $first, $whereBBB);
+        // dd($values, $count, $first, $whereBBB);
+
+        // クエリビルダを使ってデータ操作を行う方法
+        $queryBuilder = DB::table('tests')->where('text', '=', 'bbb')->select('id', 'text')->get();
+
+        dd($queryBuilder);
 
         // viewのtestフォルダのtest.blade.phpへ遷移する
         // compactを使うことで、その変数をviewに渡すことができる
