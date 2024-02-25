c6af61a8929ae244ced27ba8d66e873d3631730b
John-Thailand <naoki.honda@mirrorfit.jp>
2024-02-25 15:57:12 +0900

    お問合せ削除機能

--- laravel/task_test/app/Http/Controllers/ContactFormController.php
+++ laravel/task_test/app/Http/Controllers/ContactFormController.php
@@ -110,6 +110,9 @@ class ContactFormController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $contact = ContactForm::find($id);
+        $contact->delete();
+
+        return to_route('contacts.index');
     }
 }
