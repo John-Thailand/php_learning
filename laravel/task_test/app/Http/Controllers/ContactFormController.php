4495bcb6eb50df384304ecbb4dc4626ab349a991
John-Thailand <naoki.honda@mirrorfit.jp>
2024-02-25 15:46:48 +0900

    お問合せ編集

--- laravel/task_test/app/Http/Controllers/ContactFormController.php
+++ laravel/task_test/app/Http/Controllers/ContactFormController.php
@@ -75,7 +75,9 @@ class ContactFormController extends Controller
      */
     public function edit($id)
     {
-        //
+        $contact = ContactForm::find($id);
+
+        return view('contacts.edit', compact('contact'));
     }
 
     /**
